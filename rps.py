2d84133a127077d881ba6ab127a3dce64eae7f77
diggyblock <aparks@gmail.com>
2022-02-18 05:46:18 +0000

    game intro and object choosing

--- rps.py
+++ rps.py
@@ -1,4 +1,12 @@
 # Rock Paper Scissors Game
+objs = {"r":"ROCK", "p":"PAPER", "s":"SCISSORS"}
+game_name = "Rock, Paper, Scissors"
+game_instructions = "To make a play, type 'r', 'p', or 's'"
+game_prompt = "Rock, Paper, or Scissors?"
 
-print("Rock, Paper, Scissors!")
+print("Welcome to " + game_name)
+print(game_instructions)
+play = input(game_prompt + "\n> ")
+
+print("You chose " + objs[play])
 
